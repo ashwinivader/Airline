35ab0f00d5cc23f6a08711c0021b79b6b4b4cdef
ashwinivader <ashwinivader@outlook.com>
2023-12-22 08:57:57 +0530

    added data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name='Airline'
+version='0.0.1'
+author== "Name"
+author_email="abc."
+packages= find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
