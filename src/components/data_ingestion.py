0c2ce0f072d4dfb806928829873acc8ed620daf3
ashwinivader <ashwinivader@outlook.com>
2023-12-24 23:47:14 +0530

    modular coding

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,3 +3,59 @@
 import os
 import sys
 from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses  import dataclass
+from src.components.data_transformation import DataTransformation
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts',"train.csv")
+    test_data_path:str=os.path.join('artifacts',"test.csv")
+    raw_data_path:str=os.path.join('artifacts',"data.csv")
+
+
+class DataIngestion:
+    def __init__(self) -> None:
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion process")
+        #readinf the data...this can be normal csv or from any other data soure
+        df=pd.read_csv("data/train.csv")
+        logging.info("Read the dataset into dataframe")
+        #Making directory with name "artifacts"
+        os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+        #data.csv is original file
+        df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+        logging.info("Train test split")
+
+        #Train test split
+        train,test=train_test_split(df,test_size=0.2,random_state=42)
+        #save train data as artifacts/train.csv
+        train.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+        #save train data as artifacts/test.csv
+        test.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+        logging.info("Data injestion is completed")
+        return (self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
+
+
+        
+
+
+
+
+    
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+
+    train_data,test_data=obj.initiate_data_ingestion()
+    print(train_data,test_data)
+
+    datatransformation=DataTransformation
+    datatransformation.data_transformer_function(train_data,test_data)
+
+    #modeltrainer=ModelTrainer()
+    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,90 @@
 #This is data transformation process(encoding)
+
+import sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler, FunctionTransformer
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.linear_model import LinearRegression
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=  DataTransformationConfig()  
+
+
+    @staticmethod
+    def convert_sqft(sqft_range):
+        #logging.info("in convert sqrt")
+        if '-' in sqft_range:
+           start, end = map(float, sqft_range.split('-'))
+           return (start + end) / 2
+        elif "Sq." in sqft_range : 
+            no=str.split(sqft_range,"Sq.")[0] 
+            return (no)
+        elif "Acres" in sqft_range: 
+             no=str.split(sqft_range,"Acres")[0] 
+             return (no)
+        elif "Guntha" in sqft_range: 
+             no=str.split(sqft_range,"Guntha")[0] 
+             return (float(no)*1089) 
+        elif "Cents" in sqft_range: 
+             no=str.split(sqft_range,"Cents")[0] 
+             return (float(no)*435.56 ) 
+        elif "Perch" in sqft_range: 
+             no=str.split(sqft_range,"Perch")[0] 
+             return (float(no)*272.25 ) 
+        elif "Grounds" in sqft_range: 
+            no=str.split(sqft_range,"Grounds")[0] 
+            return (float(no)*2400 ) 
+        else:
+            return float(sqft_range)
+ 
+
+    def data_transformer_function(train_data_path,test_data_path):
+        '''
+        This function is responsible for data trnasformation
+        
+        '''
+        try:
+            numeric_features=['bath', 'balcony','total_sqft']
+            categorical_features = ['area_type', 'availability', 'location', 'size']
+            train_df=pd.read_csv(train_data_path)
+            test_df=pd.read_csv(test_data_path)
+
+# Create a preprocessor using ColumnTransformer
+            preprocessor = ColumnTransformer(
+            transformers=[
+            ('convert_sqft', FunctionTransformer(lambda x: x.map(DataTransformation.convert_sqft)), ['total_sqft']),
+            ])
+
+            # Create a pipeline with preprocessing and linear regression
+            #logging.info(train_df)
+            X=train_df[['total_sqft']]
+            y=train_df['price']
+
+            pipeline = Pipeline([
+             ('preprocessor', preprocessor),
+             ('regressor', LinearRegression())
+             ])
+
+            pipeline.fit(X, y)
+            logging.info("Information")
+            logging.info(pipeline.predict(X))
+
+
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
